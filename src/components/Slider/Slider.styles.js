586f762a6a6fa047244f25fba95dfb34cde70395
Daniel Ehrlich <ehrlichdaniel1@gmail.com>
2018-12-19 19:16:22 +0200

    first time upload to git

--- src/components/Slider/Slider.styles.js
+++ src/components/Slider/Slider.styles.js
@@ -1,53 +1,5 @@
 import styled from 'styled-components';
 
-
-// export const SliderItems = styled.div`
-//     margin: 2px;
-//     height: 122px;
-//     width: 216px;
-//     background-color:#333;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     color: white; 
-//     font-size: 24px;
-
-//     ${SliderItems}:hover & {
-//         height:200px;
-//         width: 30%;
-//         animation-duration: 2s;
-//         transition: cubic-bezier(0.86, 0, 0.07, 1);
-//     } 
-// `;
-// export const SliderContent = styled.div`
-    
-//     @media all and (min-width: 477px) {
-
-//     }
-//     @media all and (min-width: 900px) {
-//         margin-top: 0px;
-//         display: flex;
-//         flex-direction: row;
-//         align-items: center;
-//         justify-content: center;
-// }
-// `;
-// export const RowContainer = styled.div`
-    
-//     @media all and (min-width: 477px) {
-
-//     }
-//     @media all and (min-width: 900px) {
-//         display: flex;
-//         flex-wrap: wrap;
-//         justify-content: center;
-//         margin-top: 0px;
-//         height: 200px;
-//         border: 1px solid red;
-//     }
-// `;
-// ========================================
-
 export const Wrapper = styled.div`
     z-index: 3;
     @media all and (min-width: 477px) {
