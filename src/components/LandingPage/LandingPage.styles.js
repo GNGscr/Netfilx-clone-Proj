d739e83d6faa2396c7f4d1ea6ca87dc3a91a390f
Daniel Ehrlich <ehrlichdaniel1@gmail.com>
2018-12-19 18:56:11 +0200

    first time upload to git

--- src/components/LandingPage/LandingPage.styles.js
+++ src/components/LandingPage/LandingPage.styles.js
@@ -1,19 +1,23 @@
 import styled from 'styled-components';
 
 export const HeadOne = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     margin-top: 80px;
-    font-size: 53px;
+    font-size: 28px;
     font-weight: normal;
+    @media all and (min-width: 1400px) {
+        font-size: 53px;
+    }
 `;
 export const Lander = styled.div`
-    padding-top: 62px;
-    margin-left: 415px;
+    /* padding-top: 62px;
+    margin-left: 415px; */
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 500px;
-    width: 600px;
     color: white;
 `;
 export const LanderBtn = styled.div`
@@ -28,15 +32,19 @@ export const Anker = styled.a`
     justify-content: center;
     align-items: center;
     height: 218px;
-    width: 150px;
+    width: 70px;
     margin: 15px;
     color: rgba(128, 128, 128);
-    font-size: 18px;
+    font-size: 12px;
     font-weight: normal;
     text-decoration: none;
     &:hover {
         color: #fff;
   }
+  @media all and (min-width: 1400px) {
+        width: 150px;
+        font-size: 18px;
+    }
 `;
 export const ImageProfile = styled.div`
     display: flex;
@@ -44,17 +52,37 @@ export const ImageProfile = styled.div`
     align-items: center;
     border: 3px solid rgba(128, 128, 128);
     background-color: rgba(20, 20, 20);
-    height: 144px;
-    width: 144px;
+    height: 20vw;
+    width: 20vw;
     margin-bottom: 20px;
 
     &:hover {
     border: 3px solid #fff;
-  }
+    }
+    @media all and (min-width: 1400px) {
+        height: 10vw;
+        width: 10vw;
+        margin: 3vw;
+        background-color: blue;
+    }
 `;
 export const UserImg = styled.img`
-    height: 144px;
-    width: 144px;
+    height: 20vw;
+    width: 20vw;
+
+    /* @media all and (min-width: 500px) {
+        width: calc(100%/3);
+    }
+    @media all and (min-width: 800px) {
+        width: calc(100%/4);
+    }
+    @media all and (min-width: 1100px) {
+        width: calc(100%/5);
+    } */
+    @media all and (min-width: 1400px) {
+        height: 10vw;
+        width: 10vw;
+    }
 `;
 export const Button = styled.button`
     margin-top: 20px;
@@ -72,13 +100,16 @@ export const Button = styled.button`
     color: #fff;
     border: 1px solid #fff;
   }    
+  
 `;
 export const LOGO = styled.img`
     display: flex;
     width: 84px;
     height: 24px;
+    margin: 3vw;
     justify-content: flex-start;
     align-items: flex-start;
+    /* background-color: red; */
     @media all and (min-width: 477px) {
 
 }
