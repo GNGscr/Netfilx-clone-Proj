3ea6a4265f888673c5cfc32e720772447d8a0465
Daniel Ehrlich <ehrlichdaniel1@gmail.com>
2018-12-23 13:11:54 +0200

    first time upload to git

--- src/components/Global/Filter/Filter.js
+++ src/components/Global/Filter/Filter.js
@@ -7,18 +7,46 @@ export default class Filter extends Component {
 
     state = {
         InputOn: false,        
-        navbgclr: 'opacity: 0'
+        navbgclr: 'opacity: 0',
+        data: [],
+        list_data: [],
+        filter_list: [],
+
+        // original_list:data,
+        // displayed_list: data,
+        // profile_data: data[0]
     }
+
+    async componentDidMount() {
+        const res = await fetch('./MOCK_DATA.json')
+        const data = await res.json();
+        this.setState({ 
+          data
+        })
+        console.log(data.length)
+      }
+
+    // update_list_state(filtered_list){
+    //     console.log('update_list_state',filtered_list.length)
+    //     this.setState({
+    //         displayed_list: filtered_list
+    //     })
+    // }
+    // update_profile(item){
+    //     console.log('update_profile',item)
+    //     this.setState({
+    //         profile_data: item
+    //     })
+    // }
 
     update_list(e) {
         console.log(e.target.value)
-        // const { list_data, on_filter } = this.props;
-        // let txt = event.target.value;
-
-        // let filter_list = list_data.filter( item =>
-        //     item.first_name.toLowerCase().includes(txt.toLowerCase())
-        // )
-        // on_filter(filter_list)
+        const { list_data, on_filter } = this.props;
+        let txt = e.target.value;
+        let filter_list = list_data.filter( item =>
+            item.first_name.toLowerCase().includes(txt.toLowerCase())
+        )
+        on_filter(filter_list)
     }
 
     toggleInput = () => {
@@ -41,9 +69,16 @@ export default class Filter extends Component {
     }
     render() {
         return <FilterClass>
+                    {/* <Filter className="filter" list_data={this.state.original_list} on_filter={(fl)=>this.update_list_state(fl)}/> */}
                     <FilterInput bgclr={this.state.navbgclr} type="text" placeholder="Titles, people, genres" onChange={(e) => this.update_list(e)}/>
                     <FilterIcon onClick={this.toggleInput} src="https://img.icons8.com/ios-glyphs/26/000000/search.png" alt="" />
                     <FilterInputMobile type="text" placeholder="Search" onChange={(e) => this.update_list(e)}/>
+               
+                    {/* <div className="content-box">
+                    <Profile {...this.state.profile_data}/>
+                    <List list_data={this.state.displayed_list}
+                        pick={(item)=> this.update_profile(item)}/>
+                    </div> */}
                </FilterClass>
     }
 }
